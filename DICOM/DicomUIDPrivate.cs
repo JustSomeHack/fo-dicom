bfc6cfe94574af618b613965570d1a721d6662a6
DeYung Le <deyung@gmail.com>
2017-04-24 19:46:24 -0500

    Code for IO Platform

--- DICOM/DicomUIDPrivate.cs
+++ DICOM/DicomUIDPrivate.cs
@@ -5,16 +5,28 @@ namespace Dicom
 {
     public partial class DicomUID
     {
-        private static void LoadPrivateUIDs()
+        private static void LoadPrivateUIDs ()
         {
-            _uids.Add(DicomUID.GEPrivateImplicitVRBigEndian.UID, DicomUID.GEPrivateImplicitVRBigEndian);
+            _uids.Add (DicomUID.GEPrivateImplicitVRBigEndian.UID, DicomUID.GEPrivateImplicitVRBigEndian);
+            _uids.Add (DicomUID.PracticeBuilderReportText.UID, DicomUID.PracticeBuilderReportText);
+            _uids.Add (DicomUID.PracticeBuilderReportDictation.UID, DicomUID.PracticeBuilderReportDictation);
         }
 
         /// <summary>GE Private Implicit VR Big Endian</summary>
         /// <remarks>Same as Implicit VR Little Endian except for big endian pixel data.</remarks>
-        public static readonly DicomUID GEPrivateImplicitVRBigEndian = new DicomUID(
+        public static readonly DicomUID GEPrivateImplicitVRBigEndian = new DicomUID (
             "1.2.840.113619.5.2",
             "GE Private Implicit VR Big Endian",
             DicomUidType.TransferSyntax);
+
+        public static readonly DicomUID PracticeBuilderReportText = new DicomUID (
+			"1.2.826.0.1.3680043.2.93.1.0.1",
+			"Practice Builder Report Text",
+			DicomUidType.SOPClass);
+
+        public static readonly DicomUID PracticeBuilderReportDictation = new DicomUID (
+			"1.2.826.0.1.3680043.2.93.1.0.2",
+			"Practice Builder Report Dictation",
+			DicomUidType.SOPClass);
     }
 }
